b9c44226401039bf3448c1c952c52aa8216264d1
MAC <mac@MACdeMacBook-Air.local>
2026-07-25 02:04:07 +0800

    fix: address task 1 foundation review

--- alpha-server/src/test/java/io/github/onedream921/alphavue/AlphaVueApplicationTests.java
+++ alpha-server/src/test/java/io/github/onedream921/alphavue/AlphaVueApplicationTests.java
@@ -1,15 +1,27 @@
 package io.github.onedream921.alphavue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.autoconfigure.exclude="
-        + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
-        + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration,"
-        + "com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration")
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
 class AlphaVueApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
-    void contextLoads() {
+    void contextLoadsWithMigratedDataSource() {
+        Integer migrationProbeTableCount = jdbcTemplate.queryForObject(
+                "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES "
+                        + "WHERE TABLE_SCHEMA = 'public' AND TABLE_NAME = 'migration_probe'",
+                Integer.class);
+
+        assertThat(migrationProbeTableCount).isEqualTo(1);
     }
 }
